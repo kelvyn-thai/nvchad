09012213167c36a03d1527e81aebacbd9554dc46
jayce.thai <phat.tnh@carehealth.io>
2025-10-13 11:23:19 +0700

    LSP Docker / Docker compose config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,16 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "prettierd", "prettier", "tailwindcss"}
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "prettierd",
+  "prettier",
+  "tailwindcss",
+  "dockerls",
+  "docker_language_server",
+  "docker_compose_language_service",
+}
 vim.lsp.enable(servers)
 
 -- Prettier LSP configuration
@@ -11,19 +21,30 @@ vim.lsp.enable(servers)
 -- }
 
 -- Tailwind CSS LSP configuration (using modern vim.lsp.config API)
-vim.lsp.config('tailwindcss', {
+vim.lsp.config("tailwindcss", {
   filetypes_exclude = { "markdown" },
   filetypes_include = {},
   settings = {
     tailwindCSS = {
       includeLanguages = {
         elixir = "html-eex",
-        eelixir = "html-eex", 
+        eelixir = "html-eex",
         heex = "html-eex",
       },
     },
   },
 })
 
+vim.lsp.config("dockerls", {
+  settings = {
+    docker = {
+      languageserver = {
+        formatter = {
+          ignoreMultilineInstructions = true,
+        },
+      },
+    },
+  },
+})
 
 -- read :h vim.lsp.config for changing options of lsp servers
